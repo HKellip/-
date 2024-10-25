dfec3a1d5b1980e525ce6f95a2b82447007f6192
Henrique Kellip <henriqueisquierdokellip@gmail.com>
2024-10-24 21:18:15 -0300

    First commit - login

---
+++ src/components/Logo.tsx
@@ -0,0 +1,5 @@
+export default function Logo(){
+    return(
+        <div className="flex text-left">LOGO</div>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,41 @@
+//import Image from "next/image";
+//import localFont from "next/font/local";
+import Logo from "@/components/Logo";
+import { styleText } from "util";
+import Link from "next/link";
+
+
+
+function Login(){
+  return (
+    <div className=' text-center text-lg '>
+      <div className='login'>LOGIN</div>
+      <form action="teste" method="POST">
+        <p>
+          <label htmlFor="nome">Nome: </label>
+          <input type="text" id="User" name="Name" required />
+        </p>
+        <p>
+          <label htmlFor="senha">Senha: </label>
+          <input type="text" id="Senha" name="senha" required/>
+        </p>
+        <button className="text-blue-800" type="submit">Confirmar</button>
+      </form>
+      
+    </div>    
+  )
+}
+
+
+export default function Home() {
+  return (
+    <div>
+      <link rel="stylesheet" href="/teste" />
+      <Login/>
+      <Link href={"/teste"}>
+        teste
+      </Link>
+    </div>
+        
+  )
+}
