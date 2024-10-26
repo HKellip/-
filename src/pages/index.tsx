7e745f4de0c245475121012637053be29afe6ed7
Alberto Salazar Iense <albertolense@t-upsolucoes.com.br>
2024-10-26 17:41:04 -0300

    add: aula 1 como gerenciar input e componentes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,5 @@
-//import Image from "next/image";
-//import localFont from "next/font/local";
-import Logo from "@/components/Logo";
-import { styleText } from "util";
-import Link from "next/link";
-
-
-
-function Login(){
-  return (
-    <div className=' text-center text-lg pt-16'>
-      <div className='login'>LOGIN</div>
-      <form action="teste" method="POST">
-        <p>
-          <label htmlFor="nome">Nome: </label>
-          <input type="text" id="User" name="Name" required />
-        </p>
-        <p>
-          <label htmlFor="senha">Senha: </label>
-          <input type="text" id="Senha" name="senha" required/>
-        </p>
-        <button className="text-blue-800" type="submit">Confirmar</button>
-      </form>
-      
-    </div>    
-  )
-}
-
-
 export default function Home() {
   return (
-    <div>
-      <Logo/>
-      <Login />
-      <Link href={"/teste"}>
-        teste
-      </Link>
-    </div>
-        
+    <></>
   )
 }
